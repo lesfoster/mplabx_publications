510da48d0ea3a8183f275b34ac16fc24c3f0701a
Leslie Foster <lfoster.se.be@gmail.com>
2024-12-25 13:41:54 -0500

    Flexibility added for DHT11 pins

---
+++ dht11_lib.X/dht11_overrides.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   dht11_overrides.h
+ * Author: lesfo
+ *
+ * Created on December 25, 2024, 1:16 PM
+ */
+
+#ifndef DHT11_OVERRIDES_H
+#define	DHT11_OVERRIDES_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+// This may be overridden or commented out per your needs
+#define DHT_PIN_RA4 1
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DHT11_OVERRIDES_H */
+
